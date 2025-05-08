f2a199b9bc003190e36a4fa8a140300497b55cf2
Nirojan Arunaparan <nirojan.arun@gmail.com>
2025-05-08 17:21:07 -0400

    Add TodoItem class

---
+++ Models/TodoItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ToDoListApplication.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime? DueDate { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public TodoItem(int id, string title, string description, DateTime? dueDate)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            DueDate = dueDate;
+            IsCompleted = false;
+            CreatedAt = DateTime.Now;
+        }
+    }
+}
